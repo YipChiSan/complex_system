db3f141dc516dfa9da9fb97b0a3eeaea73832fc8
YipChiSan <zye39@yahoo.com>
2021-03-21 13:09:33 +1100

    Naive version of solution 1 (not tested)

--- assignment 1/Shuttle.java
+++ assignment 1/Shuttle.java
@@ -1,22 +1,39 @@
+import javax.print.attribute.standard.Destination;
+
 /**
  * A shuttle is to pass a vial from one place to another
  */
 
- public class Shuttle {
-    protected Destination[] destinations;
+ public class Shuttle extends VaccineHandlingThread {
+    protected Destination start;
+    protected Destination destination;
 
     /**
-     * Create a new Shuttle to transfer vial to one place to another 
+     * Create a new Shuttle to transfer vial from start to destination. 
      * @param destinations
      * @throws IllegalArgumentException
      */
-    public Shuttle(Destination[] destinations) 
-        throws IllegalArgumentException{
-        if (destinations.length != 2){ 
-            throw new IllegalArgumentException();
-        } else{
-            this.destinations = destinations;
-        }  
+    public Shuttle(Destination start, Destination destination) {
+        super();
+        this.start = start;
+        this.destination = destination;      
+    }
+
+    /**
+     * Loop infinitely to take vial from start tp destination
+     */
+    public void run(){
+        while (!isInterrupted()){
+            try{
+                holdingVial = start.getVialByShuttle();
+                destination.putVialByShuttle(holdingVial);
+                holdingVial = destination.getVialByShuttle();
+                start.putVialByShuttle(holdingVial);
+            } catch (InterruptedException e){
+                this.interrupt();
+            }
+        }
+        System.out.println("Shuttle terminated");
     }
 
 
